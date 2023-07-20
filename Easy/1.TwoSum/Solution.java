3bbc541fcf128730fbd59909c06d3632f0230e73
dj1095 <durgapramodh10dj95@gmail.com>
2023-07-20 10:22:37 -0500

    changed directory name

--- Easy/1.TwoSum/Solution.java
+++ Easy/1.TwoSum/Solution.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
-class TwoSum {
+class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer,Integer> map = new HashMap<>();
         for(int i=0;i<nums.length;i++){
