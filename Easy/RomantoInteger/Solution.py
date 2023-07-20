636fac04f7f1df013a9f4f728101635ebe615aaa
dj1095 <durgapramodh10dj95@gmail.com>
2023-07-20 01:45:32 -0500

    easy problem adde

---
+++ Easy/RomantoInteger/Solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        romanMap = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+
+        result = 0
+        for i in range(len(s)):
+            if i+1 < len(s) and romanMap[s[i]] < romanMap[s[i+1]] :
+                result +=  - romanMap[s[i]]
+            else:
+                result += romanMap[s[i]]
+        return result
+
+
