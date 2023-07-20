d831addaa4ff9f19256d4bdcb0e8f173b1bbac5f
dj1095 <durgapramodh10dj95@gmail.com>
2023-07-20 00:17:59 -0500

    Testing with Script

---
+++ scripts/LeetcodeScript.py
@@ -0,0 +1,60 @@
+import requests
+import json
+import os
+
+BASE_URL = "https://leetcode.com/problems"
+QUESTION_NAME = "redundant-connection"
+# Define the GraphQL query with variables
+query = '''
+  query questionContent($titleSlug: String!){
+    question(titleSlug: $titleSlug){
+    questionId
+    questionTitle
+    difficulty
+    content
+    mysqlSchemas
+    dataSchemas
+    }
+}
+'''
+
+# Define the GraphQL endpoint URL
+graphql_endpoint = 'https://leetcode.com/graphql'
+
+# Define the variables for the query
+variables = {
+    "titleSlug": QUESTION_NAME
+}
+
+# Make a POST request to the GraphQL server
+response = requests.post(graphql_endpoint, json={'query': query, 'variables': variables})
+
+# Parse the response
+data = json.loads(response.text)
+
+
+#File Handling
+file_name = "README.md"
+link = BASE_URL + "/" + QUESTION_NAME + "/"                                   
+question_id = data["data"]["question"]["questionId"]
+question_title = data["data"]["question"]["questionTitle"]
+difficulty = data["data"]["question"]["difficulty"]
+content = data["data"]["question"]["content"]
+resp_text = (content.replace("\\n"," ")
+                    .replace("\\t","")
+                    .replace('=\\', '='))
+
+#Create new Directory
+title = question_title.replace(" ", "")
+dir_path = os.path.dirname(os.getcwd()) + "/" + difficulty + "/" + title
+file_path = dir_path + "/" + file_name
+
+html_content = f'''
+<h2>Difficulty - {difficulty}</h2>
+<h2><a href="{link}" target="_blank">{question_id}.{question_title}</a></a></h2>
+'''
+
+file_content = "".join([html_content,"\n",resp_text])
+f = open(file_path,'w')
+f.write(file_content)
+
